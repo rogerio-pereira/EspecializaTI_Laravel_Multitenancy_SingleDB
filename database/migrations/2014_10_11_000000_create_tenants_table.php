fbdc44ac1f17417bb0f1576c96a2a4e9451630c3
Rogerio Pereira <hi@rogeriopereira.dev>
2021-05-25 21:49:21 -0400

    04 - Configurando UUID no Laravel

--- database/migrations/2014_10_11_000000_create_tenants_table.php
+++ database/migrations/2014_10_11_000000_create_tenants_table.php
@@ -16,6 +16,7 @@ class CreateTenantsTable extends Migration
         Schema::create('tenants', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->unique();
+            $table->uuid('uuid');
             $table->timestamps();
         });
     }
