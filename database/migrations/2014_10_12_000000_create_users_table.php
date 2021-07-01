f596b03ad0919d1a0a5f36f393342f72725be95e
Rogerio Pereira <hi@rogeriopereira.dev>
2021-06-30 20:49:29 -0400

    02 - Tenants e Relacionamentos no Laravel e auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,14 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedInteger('tenant_id');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('tenant_id')
+                ->references('id')
+                ->on('tenats')
+                ->onDelete('cascade');
         });
     }
 
